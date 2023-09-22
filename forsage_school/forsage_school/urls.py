83d655c0b4a1da530c770b73241e2cbc97c3cf4a
Maxim7777ma <maximpetrusenko05@gmail.com>
2023-09-22 17:36:36 +0300

    Good2

--- forsage_school/forsage_school/urls.py
+++ forsage_school/forsage_school/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 from courses import views  
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.about, name='about'), 
